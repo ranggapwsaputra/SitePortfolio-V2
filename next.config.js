b7a74f7825bf23d8e9028402eb38bf36ea563345
ranggapwsaputra <rangga.pw.saputra@gmail.com>
2025-08-16 12:00:59 +0700

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
-  images: {
-    unoptimized: true, 
-  },
-  basePath: '/Portfolio-v2', // Nama repo GitHub lo
-  assetPrefix: '/Portfolio-v2/',
-};
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
